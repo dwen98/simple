d2f7a57c629de741bdfe6c3e44d2635d4ddd71ed
dwen98 <david.d.wen@gmail.com>
2023-08-22 21:45:11 +1000

    push to gitHub

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Simple</title>
+</head>
+<body>
+    <h1>
+        <?php
+          echo 'Hello World!';
+
+          print "<p>ABC testing</p>";
+        ?>
+    </h1>
+
+</body>
+</html>
